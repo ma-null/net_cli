e2ef266efb05a211607cd7ed6752ca8605895232
ma-null <maypachulli@gmail.com>
2019-03-28 02:48:56 +0300

    add tests and made more abstract func

---
+++ request/common_test.go
@@ -0,0 +1,26 @@
+package request
+
+import (
+	"net/http"
+)
+
+func MockParams() Params {
+	return Params{
+		"v1",
+		"127.0.0.1",
+		"8080",
+	}
+}
+
+type mockServer struct {
+	pr Params
+	mockGet func(requestPath string) (*http.Response, error)
+}
+
+func NewMockServer(pr Params, mockGet func(requestPath string) (*http.Response, error)) Server {
+	return &mockServer{pr, mockGet}
+}
+
+func (s *mockServer) Get(requestPath string) (*http.Response, error) {
+	return s.mockGet(requestPath)
+}
